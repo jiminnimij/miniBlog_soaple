4f842a4293fe9e962b53afba0f77355c1d31318d
jimin <010709min@ewhain.net>
2023-10-23 19:59:28 +0900

    Button error is modified

--- src/component/ui/Button.jsx
+++ src/component/ui/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-const styledButton = styled.button`
+const StyledButton = styled.button`
   padding: 8px 16px;
   font-size: 16px;
   border-width: 1px;
@@ -12,7 +12,7 @@ const styledButton = styled.button`
 function Button(props){
   const { title, onClick } = props;
   
-  return <styledButton onClick={onClick}>{title || "button"}</styledButton>
+  return <StyledButton onClick={onClick}>{title || "button"}</StyledButton>
 }
 
 export default Button;
